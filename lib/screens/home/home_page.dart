8fb0154a5bd8d1beafebb01faa1390c08c8dc597
karemthabet <karemsabit.com>
2025-04-09 23:25:01 +0200

    add feature next and prev tasbeh

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:sebha_app/utils/colors/app_colors.dart';
 import 'package:sebha_app/utils/constants/app_constants.dart';
 import 'package:sebha_app/utils/styles/app_styles.dart';
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,107 +9,50 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+
+
 class _HomePageState extends State<HomePage> {
   int _counter = 0;
-  String _currentTasbeeh = AppConstants.tasbih[0];
+  int _round = 0;
+  int _currentTasbeehIndex = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.whiteColor,
-      body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(height: 30),
-
-            Text(
-              "السبحة الالكترونية",
-              style: AppStyles.titleTextStyle
-            ),
-
-            const SizedBox(height: 20),
-
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 25),
-              padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
-              decoration: BoxDecoration(
-                color: AppColors.primaryColor.withOpacity(0.3),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    textAlign: TextAlign.center,
-                    _currentTasbeeh,
-                    style: AppStyles.titleTextStyle
-                  ),
-                  const SizedBox(height: 25),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      IconButton(
-                        onPressed: _resetCounter,
-                        icon: const Icon(Icons.refresh, color: AppColors.primaryColor),
-                      ),
-                      IconButton(
-                        onPressed: _openTasbeehSelector,
-                        icon: const Icon(Icons.menu, color: AppColors.primaryColor),
-                      ),
-                     
-                    ],
-                  ),
-                ],
-              ),
-            ),
-
-            const SizedBox(height: 40),
-
-            GestureDetector(
-              onTap: _incrementCounter,
-              child: CircleAvatar(
-                radius: 75,
-                backgroundColor: AppColors.primaryColor.withOpacity(0.2),
-                child: CircleAvatar(
-                  radius: 70,
-                  backgroundColor: AppColors.primaryColor,
-                  child: Text(
-                    _counter.toString(),
-                    style: AppStyles.subTitleTextStyle
-                  ),
-                ),
-              ),
-            ),
-
-            const Spacer(flex: 2),
-          ],
-        ),
-      ),
-    );
-  }
+  String get _currentTasbeeh => AppConstants.tasbih[_currentTasbeehIndex];
 
   void _incrementCounter() {
     setState(() {
       _counter++;
-     
+      if (_counter == 33) {
+        _round++;
+        _counter = 0;
+      }
     });
   }
 
   void _resetCounter() {
     setState(() {
       _counter = 0;
+      _round = 0;
     });
   }
 
-  void _changeTasbeeh(String newTasbeeh) {
+  void _changeTasbeeh(int newIndex) {
     setState(() {
-      _currentTasbeeh = newTasbeeh;
+      _currentTasbeehIndex = newIndex;
       _counter = 0;
+      _round = 0;
     });
-    Navigator.pop(context);
   }
 
-  
+ void _nextTasbeeh() {
+  int nextIndex = (_currentTasbeehIndex + 1) % AppConstants.tasbih.length;
+  _changeTasbeeh(nextIndex);
+}
+
+void _previousTasbeeh() {
+  int prevIndex = (_currentTasbeehIndex - 1 + AppConstants.tasbih.length) % AppConstants.tasbih.length;
+  _changeTasbeeh(prevIndex);
+}
+
 
   void _openTasbeehSelector() {
     showModalBottomSheet(
@@ -121,7 +63,11 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (context, index) {
           final tasbeeh = AppConstants.tasbih[index];
           return TextButton(
-            onPressed: () => _changeTasbeeh(tasbeeh),
+            onPressed: () {
+               _changeTasbeeh(index);
+              Navigator.pop(context);
+
+            },
             child: Container(
               padding: const EdgeInsets.symmetric(vertical: 12),
               margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
@@ -139,6 +85,96 @@ class _HomePageState extends State<HomePage> {
             ),
           );
         },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 30),
+            Text("السبحة الالكترونية", style: AppStyles.titleTextStyle),
+            const SizedBox(height: 20),
+
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 25),
+              padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: _previousTasbeeh,
+                        icon: const Icon(Icons.chevron_left, size: 32),
+                      ),
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            _currentTasbeeh,
+                            style: AppStyles.titleTextStyle,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: _nextTasbeeh,
+                        icon: const Icon(Icons.chevron_right, size: 32),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 25),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        onPressed: _resetCounter,
+                        icon: const Icon(Icons.refresh, color: AppColors.primaryColor),
+                      ),
+                      IconButton(
+                        onPressed: _openTasbeehSelector,
+                        icon: const Icon(Icons.menu, color: AppColors.primaryColor),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 40),
+
+            GestureDetector(
+              onTap: _incrementCounter,
+              child: CircleAvatar(
+                radius: 75,
+                backgroundColor: AppColors.primaryColor.withOpacity(0.2),
+                child: CircleAvatar(
+                  radius: 70,
+                  backgroundColor: AppColors.primaryColor,
+                  child: Text(
+                    "$_counter/33",
+                    style: AppStyles.subTitleTextStyle,
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Text("Round $_round", style: AppStyles.titleTextStyle),
+
+            const Spacer(flex: 2),
+          ],
+        ),
       ),
     );
   }
